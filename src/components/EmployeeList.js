a81fbc539461415556dad2c0a1bcb5907be6e9f4
krishna0379 <iamkrishna0379@gmail.com>
2021-06-03 16:27:56 +0530

    emp setup

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,7 @@
+export function EmployeeList() {
+  return (
+    <div>
+      <h1>Employee Listing Page</h1>
+    </div>
+  );
+}
---
+++ src/components/EmployeeUpsert.js
@@ -0,0 +1,7 @@
+export function EmployeeUpsert() {
+  return (
+    <div>
+      <h1>Create Employee/ Update Employee Page</h1>
+    </div>
+  );
+}
